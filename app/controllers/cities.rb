7fafa988ff57af59fa18b0d54e6ead311a0fc78e
Kara A Carrell & Michael Chu <pair+KaraAJC.mchu0010@devbootcamp.com>
2015-03-02 17:45:21 -0600

    adding view to cities resource

--- app/controllers/cities.rb
+++ app/controllers/cities.rb
@@ -1,8 +1,9 @@
-get '/cities' do
-  @cities = Cities.all
-  erb :'cities/index'
-end
+# get '/cities' do
+#   @cities = Cities.all
+#   erb :'cities/index'
+# end
 
 get '/cities/:city_id' do
   @city = City.find(params[:city_id])
+  erb:'cities/show'
 end
